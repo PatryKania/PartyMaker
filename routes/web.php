428e1e6c238c4e76b946ac1974f75950849e005a
PatryKania <patryk.kania001@gmail.com>
2025-11-29 16:01:40 +0100

    UPDATE: change main page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\GoogleLoginController;
 use App\Http\Controllers\Auth\FacebookLoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
 
 
